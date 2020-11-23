bce0f92a32c62515138182164c6c1eb9f1055564
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-11-23 15:34:16 +0545

    added videoview on home screen with play pause support

--- app/src/main/java/com/halfwaiter/lol/fragments/HomeFragment.java
+++ app/src/main/java/com/halfwaiter/lol/fragments/HomeFragment.java
@@ -3,10 +3,12 @@ package com.halfwaiter.lol.fragments;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.VideoView;
 
 import com.halfwaiter.lol.R;
 
@@ -16,6 +18,28 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+        final VideoView videoView = view.findViewById(R.id.myvideoview);
+
+        String videoPath = "android.resource://" + getContext().getPackageName() + "/" + R.raw.next_video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        videoView.start();
+        videoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (videoView.isPlaying())
+                {
+                    System.out.println("asdbjh");
+                    videoView.pause();
+                }
+                else {
+                    System.out.println("skjadbhjsdh");
+                    videoView.start();
+                }
+            }
+        });
+        videoView.setMediaController(null);
+        return view;
     }
 }
--- app/src/main/java/com/halfwaiter/lol/fragments/SearchFragment.java
+++ app/src/main/java/com/halfwaiter/lol/fragments/SearchFragment.java
@@ -3,10 +3,16 @@ package com.halfwaiter.lol.fragments;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.halfwaiter.lol.R;
 
@@ -27,6 +33,25 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+//        VideoView videoView = view.findViewById(R.id.myvideoview);
+//        String videoPath = "android.resource://" + getContext().getPackageName() + "/" + R.raw.next_video;
+//        Uri uri = Uri.parse(videoPath);
+//        videoView.setVideoURI(uri);
+//        videoView.start();
+//
+//        videoView.setMediaController(null);
+//        MediaController mediaController = new MediaController(getContext());
+//        videoView.setMediaController(mediaController);
+//        mediaController.setAnchorView(videoView);
+
+//        android.widget.LinearLayout.LayoutParams params = (android.widget.LinearLayout.LayoutParams) videoView.getLayoutParams();
+//        int width = getResources().getConfiguration().screenWidthDp;
+//        int height = getResources().getConfiguration().screenHeightDp;
+//        System.out.println("jbsajhasdh"+ width+ height);
+//        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){}
+//        params.leftMargin = 0;
+//        videoView.setLayoutParams(params);
+        return view;
     }
 }
